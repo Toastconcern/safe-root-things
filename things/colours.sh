3b1a51df7430147f3eed3c75b89d443bea8f6d55
cats4606 <84914492+ilovecats4606@users.noreply.github.com>
2025-08-16 09:31:46 +1000

    Create colours.sh

---
+++ things/colours.sh
@@ -0,0 +1,33 @@
+#!/system/bin/sh
+
+RED_LED="/sys/class/leds/red/brightness"
+GREEN_LED="/sys/class/leds/green/brightness"
+BLUE_LED="/sys/class/leds/blue/brightness"
+
+set_rgb() {
+    echo "$1" > "$RED_LED"
+    echo "$2" > "$GREEN_LED"
+    echo "$3" > "$BLUE_LED"
+}
+
+clamp() {
+    if [ "$1" -lt 0 ]; then echo 0
+    elif [ "$1" -gt 255 ]; then echo 255
+    else echo "$1"
+    fi
+}
+
+while true; do
+    for i in $(seq 0 255); do
+        set_rgb $(clamp $((255 - i))) $(clamp $i) 0       # Red to Green
+        sleep 0.01
+    done
+    for i in $(seq 0 255); do
+        set_rgb 0 $(clamp $((255 - i))) $(clamp $i)       # Green to Blue
+        sleep 0.01
+    done
+    for i in $(seq 0 255); do
+        set_rgb $(clamp $i) 0 $(clamp $((255 - i)))       # Blue to Red
+        sleep 0.01
+    done
+done
